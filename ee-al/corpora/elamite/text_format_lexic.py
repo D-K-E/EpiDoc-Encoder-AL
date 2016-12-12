a22efb8defd178813fea16a93e5c40bf6a92d58e
D-K-E <kaaneraslan@gmail.com>
2016-12-12 23:18:48 +0100

    Changed folder name anl to al

---
+++ ee-al/corpora/elamite/text_format_lexic.py
@@ -0,0 +1,121 @@
+# Achemenide Elamite Determinatives / Hakamanişiya elamcasındaki belirticiler ---------------------------
+
+# Prepositioned determinatives / İsmin önüne gelen belirticiler
+
+Ach_El_dets_prepos = [
+    "DIŠ", # for male person / erkek kişiler için
+    "AŠ", # for locations / yer, bölge vs için
+    "DINGIR", # for divine names / ilahi isimler için
+    "AN", # another reading of dingir / dingir işaretinin başka bir okunuşu
+    "HAL", # for country, region names / ülke bölge adları için
+    "GIŠ", # for trees and vegetables and other materials / ağaç, bitki ve diğer malzemeler için
+    "SAL", # for female names / kadın isimleri için
+    "URU", # rare for city names / nadiren şehir isimleri için
+    "KUR" # for country and mountain names / ülke ve dağ isimleri için
+]
+
+# Post positioned determinatives / İsmin arkasına gelen belirticiler
+
+Ach_El_dets_postpos = [
+    "MEŠ", # for plurals or logograms / çoğul isimler veya kelime-yazı işaretleri için
+    "KAM", # for ordinals / sıra sayılar için
+    "KÁM", # for ordinals / sıra sayılar için
+    "KI" # for country and city names / ülke ve şehir adları için
+]
+
+# Logograms / Kelime-yazı işaretleri -----------------
+
+Elam_Logos = [#
+    "A", #
+    "AB", #
+    "ANŠE", #
+    "BA", #
+    "BABBAR", #
+    "BAN", #
+    "BAR", #
+    "BAT", #
+    "BI", #
+    "BÀD", #
+    "DA", #
+    "DIL", #
+    "DUB", #
+    "DUMU", #
+    "DUR", #
+    "ERIN", #
+    "EŠŠANA", #
+    "GAB", #
+    "GAL", #
+    "GAM", #
+    "GI", #
+    "GIBÍL", #
+    "GU", #
+    "GUD", #
+    "GÚ", #
+    "GÜ", #
+    "HAR", #
+    "HU", #
+    "I", #
+    "IB", #
+    "II", #
+    "III", #
+    "IL", #
+    "IM", #
+    "ITI", #
+    "ITU", #
+    "KASKAL", #
+    "KIMIN", #
+    "KUŠ", #
+    "KÙ", #
+    "KÙŠ", #
+    "LA", #
+    "LAK", #
+    "LAM", #
+    "MA", #
+    "MAL", #
+    "MAN", #
+    "MAR", #
+    "ME", #
+    "MIN", #
+    "MU", #
+    "MÁ", #
+    "MÙŠ", #
+    "NA", #
+    "NIM", #
+    "NIN", #
+    "NINNI", #
+    "NITA", #
+    "NUMUN", #
+    "NUN", #
+    "PA", #
+    "PAD", #
+    "PAP", #
+    "PI", #
+    "PÍR", #
+    "RA", #
+    "RUH", #
+    "SAG", #
+    "SIG4", #
+    "SUNKI", #
+    "SUNKIR", #
+    "TI", #
+    "TUK", #
+    "TUR", #
+    "TUŠ", #
+    "TÚG", #
+    "UD", #
+    "UDU", #
+    "UH", #
+    "UN", #
+    "URU", #
+    "UTU", #
+    "ZÌ" #
+#
+]
+
+# Elamite Logograms with Post positioned Determinatives / Elamca kelime-yazı işaretleri ile isimden sonra gelen belirticiler
+
+el_logos_postpos = []
+
+
+
+
